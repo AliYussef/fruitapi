f824d143e32863335a10ebc6710359f2fe5e75e0
aliyussef <a.yussef@outlook.com>
2021-03-21 21:58:04 +0100

    Initial commit

---
+++ src/main/java/ay/springframework/fruitapi/FruitapiApplication.java
@@ -0,0 +1,13 @@
+package ay.springframework.fruitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitapiApplication.class, args);
+    }
+
+}
